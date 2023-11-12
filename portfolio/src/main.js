4d98bb10df12a250557a7da53bef42777829f1ad
yujindev <yujin.front@gmail.com>
2023-11-12 17:30:00 +0900

    JS-ArrowUp

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -14,4 +14,14 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll',()=>{
   home.style.opacity = 1 - window.scrollY / homeHeight;
-})
+});
+
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
